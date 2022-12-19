f7670f30a09267d0ef6001ef5edf09b2081f1f5a
Jayme Anchante <jayme.anchante@disroot.org>
2022-12-18 23:29:10 -0300

    index - changed a bit the wording and the size of the carousel

--- js/carousel.js
+++ js/carousel.js
@@ -4,7 +4,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
       rewind: true,
       pagination: false,
       cover: true,
-      heightRatio: 0.9,
+      heightRatio: 0.8,
       arrows: false,
       autoplay: true,
       interval: 3000
