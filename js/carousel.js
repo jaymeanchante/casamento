52a32d0d2e558edd1ea183735effd4b5dc175aec
Jayme Anchante <jayme.anchante@disroot.org>
2022-12-18 21:18:00 -0300

    index - changed image gallery to a carousel

---
+++ js/carousel.js
@@ -0,0 +1,14 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide(
+    '#image-carousel', {
+      rewind: true,
+      pagination: false,
+      cover: true,
+      heightRatio: 0.75,
+      arrows: false,
+      autoplay: true,
+      interval: 3000
+      }
+    ).mount();
+  }
+);
